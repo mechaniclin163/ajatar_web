a5f807408a55c224445a552bb29c4bb8d28ef06f
xulin <xulin@chukong-inc.com>
2020-07-06 14:42:10 +0800

    change router

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://jdi.cocosbcx.net/dmd/',
+    assetsPublicPath: 'https://jdi.cocosbcx.net/gamesubmit/',
 
     /**
      * Source Maps
